f9f3ba2d992ad9c0e68318adfc9eda247f509e26
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2019-10-10 15:55:45 -0400

    Write Python code using multi-level inheritance.

---
+++ Exp4_c.py
@@ -0,0 +1,20 @@
+class class1:
+    def data1(sel):
+        sel.a=10;
+class class2(class1):
+    def data2(sel):
+        sel.data1();
+        sel.b = 20 + sel.a;
+        
+class multi_inher(class2):
+    def data3(sel):
+        sel.data1();
+        sel.data2();
+        sel.result=20+sel.b
+    def out(sel):
+        print "Result => " + str(sel.result)
+        
+Student1=multi_inher();
+Student1.data3();
+Student1.out();
+
